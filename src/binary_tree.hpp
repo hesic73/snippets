a12b1f190d61d72d2f4aefb4fb12a7090166e99a
hesic73 <hesicheng2001@163.com>
2024-02-12 16:55:55 -0800

    add some comments for binary_tree

--- src/binary_tree.hpp
+++ src/binary_tree.hpp
@@ -20,6 +20,14 @@ namespace hsc_snippets
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
 
+    /**
+     * Constructs a binary tree from a vector of optional integers.
+     *
+     * @param v A vector of std::optional<int>, where each element represents a potential node in the
+     *          binary tree. An std::nullopt value indicates the absence of a node at that position.
+     * @return A pointer to the root node of the newly constructed binary tree. Returns nullptr if
+     *         the input vector is empty.
+     */
     TreeNode *new_binary_tree(const std::vector<std::optional<int>> v)
     {
         if (v.empty())
@@ -61,6 +69,13 @@ namespace hsc_snippets
         return root;
     }
 
+    /**
+     * Deletes all nodes of a binary tree to free memory.
+     *
+     * @param root A pointer to the root node of the binary tree. If the pointer is nullptr, the
+     *             function does nothing, safely handling the case of an empty tree or reaching
+     *             the end of a branch.
+     */
     void delete_binary_tree(TreeNode *root)
     {
         if (root == nullptr)
@@ -68,6 +83,23 @@ namespace hsc_snippets
         delete_binary_tree(root->left);
         delete_binary_tree(root->right);
         delete root;
+    }
+
+    /**
+     * Calculates the depth of a binary tree.
+     *
+     * @param root A pointer to the root node of the binary tree. If the tree is empty (nullptr),
+     *             the depth is considered to be 0.
+     * @return The depth of the binary tree as an integer. An empty tree has a depth of 0.
+     */
+    int get_binary_tree_depth(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int depth = 1;
+        depth = std::max(depth, get_binary_tree_depth(root->left) + 1);
+        depth = std::max(depth, get_binary_tree_depth(root->right) + 1);
+        return depth;
     }
 
     void inorder(TreeNode *root, std::function<void(int)> func)
@@ -98,7 +130,16 @@ namespace hsc_snippets
         func(root->val);
     }
 
-    // elements of the binary tree should be unique
+    /**
+     * Converts a binary tree to an adjacency list representation. All node values in the binary tree must be unique.
+     * The adjacency list is bidirectional, meaning for each parent-child relation,
+     * both the parent's list contains the child and the child's list contains the parent.
+     *
+     * @param root A pointer to the root node of the binary tree. If the tree is empty (nullptr),
+     *             the function returns an empty adjacency list.
+     * @return An unordered_map where keys are node values and values are vectors of integers
+     *         representing the node values of adjacent nodes.
+     */
     std::unordered_map<int, std::vector<int>> binary_tree_to_adjacency_list(TreeNode *root)
     {
         auto adjacencyList = std::unordered_map<int, std::vector<int>>{};
