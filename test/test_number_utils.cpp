72b603ca1c7d2c90e4e3dfe62a099f2365ff9af1
hesic73 <hesicheng20@gmail.com>
2024-02-25 00:43:50 -0800

    sieve

--- test/test_number_utils.cpp
+++ test/test_number_utils.cpp
@@ -20,4 +20,26 @@ TEST_CASE("number_utils.hpp", )
         REQUIRE(numBits((1 << 8) - 1) == 8);
         REQUIRE(numBits(static_cast<std::int32_t>(-12345)) == 32);
     }
+    SECTION("gcd")
+    {
+        REQUIRE(gcd(6, 4) == 2);
+        REQUIRE(gcd(4, 6) == 2);
+        REQUIRE(gcd(8, 9) == 1);
+        REQUIRE(gcd(24, 36) == 12);
+    }
+    SECTION("lcm")
+    {
+        REQUIRE(lcm(6, 4) == 12);
+        REQUIRE(lcm(4, 6) == 12);
+        REQUIRE(lcm(8, 9) == 72);
+        REQUIRE(lcm(24, 36) == 72);
+    }
+
+    SECTION("SieveOfEratosthenes"){
+        auto primes = SieveOfEratosthenes(100);
+        auto expected_primes=vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                                         31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                                         73, 79, 83, 89, 97};
+        REQUIRE(primes==expected_primes);
+    }
 }
