efa601271e4aefbecc6d4e284c547ee5bda6d13b
hesic73 <hesic73@users.noreply.github.com>
2024-07-15 22:01:07 +0000

    deploy: 27b588f97c710afe5cacd8181ef15051ad7087da

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['binary_5fsearch_0',['binary_search',['../namespacehsc__snippets.html#ab19a8c1f33cd0fbc24e3ea9d093430d1',1,'hsc_snippets']]],
   ['binary_5ftree_5fto_5fadjacency_5flist_1',['binary_tree_to_adjacency_list',['../namespacehsc__snippets.html#a8052840df1ce8e295a2d69d656d836d5',1,'hsc_snippets']]],
-  ['breadth_5ffirst_5fsearch_2',['breadth_first_search',['../namespacehsc__snippets.html#a94bf74759d725b5eaffb0609b370eecc',1,'hsc_snippets::breadth_first_search(std::vector&lt; std::vector&lt; int &gt; &gt; &amp;adjacency_list, int root, std::function&lt; void(int, int)&gt; callback)'],['../namespacehsc__snippets.html#aefa53f26ca29b00677b58dfbd3957fb6',1,'hsc_snippets::breadth_first_search(std::unordered_map&lt; int, std::vector&lt; int &gt; &gt; &amp;adjacency_list, int root, std::function&lt; void(int, int)&gt; callback)']]]
+  ['binary_5ftree_5fto_5fvector_2',['binary_tree_to_vector',['../namespacehsc__snippets.html#ab383720208008a636de2d2fdaae6f79d',1,'hsc_snippets']]],
+  ['breadth_5ffirst_5fsearch_3',['breadth_first_search',['../namespacehsc__snippets.html#a94bf74759d725b5eaffb0609b370eecc',1,'hsc_snippets::breadth_first_search(std::vector&lt; std::vector&lt; int &gt; &gt; &amp;adjacency_list, int root, std::function&lt; void(int, int)&gt; callback)'],['../namespacehsc__snippets.html#aefa53f26ca29b00677b58dfbd3957fb6',1,'hsc_snippets::breadth_first_search(std::unordered_map&lt; int, std::vector&lt; int &gt; &gt; &amp;adjacency_list, int root, std::function&lt; void(int, int)&gt; callback)']]]
 ];
