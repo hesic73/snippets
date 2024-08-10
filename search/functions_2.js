5f3f3d0b2781999fb578ccc23b497c3bc20d972a
hesic73 <hesic73@users.noreply.github.com>
2024-08-10 03:23:47 +0000

    deploy: 6537af8517d52ab7545881ec5ea50b89bb6b808a

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['containsinrange_0',['containsInRange',['../namespacehsc__snippets.html#a2f76e1d412f83567b03ef84aa374878a',1,'hsc_snippets']]]
+  ['containsinrange_0',['containsInRange',['../namespacehsc__snippets.html#a2f76e1d412f83567b03ef84aa374878a',1,'hsc_snippets']]],
+  ['count_5fconnected_5fcomponents_1',['count_connected_components',['../namespacehsc__snippets.html#ac2f14c8a776c8904760e16d32d360d1e',1,'hsc_snippets']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['factorial_0',['factorial',['../classhsc__snippets_1_1BigInteger.html#a5854b777ed3c6a5596c375f40c73ec0b',1,'hsc_snippets::BigInteger']]],
   ['find_1',['find',['../classhsc__snippets_1_1UnionFind.html#a07e01f552b4663ce91983eab05657ceb',1,'hsc_snippets::UnionFind']]],
-  ['find_5feuler_5fpath_5fdirected_2',['find_euler_path_directed',['../namespacehsc__snippets.html#a50a72d9be388b969808af9cd6617aea9',1,'hsc_snippets']]],
-  ['findconnectedcomponents_3',['findConnectedComponents',['../namespacehsc__snippets.html#a16dd678bffb7adede9bc2eabe0e92665',1,'hsc_snippets']]],
+  ['find_5fconnected_5fcomponents_2',['find_connected_components',['../namespacehsc__snippets.html#af6df815c2f70c261770b2f2eacde7489',1,'hsc_snippets']]],
+  ['find_5feuler_5fpath_5fdirected_3',['find_euler_path_directed',['../namespacehsc__snippets.html#a50a72d9be388b969808af9cd6617aea9',1,'hsc_snippets']]],
   ['from_5finteger_4',['from_integer',['../classhsc__snippets_1_1BigInteger.html#afe03ef25178043927498952a690c8669',1,'hsc_snippets::BigInteger']]]
 ];
