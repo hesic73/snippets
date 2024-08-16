e489becb1245679162a7c76043b67a52f1361828
hesic73 <hesic73@users.noreply.github.com>
2024-08-16 21:54:30 +0000

    deploy: 402756b80d5906f4ad5d02fa3a4403ed9f58b038

--- search/all_8.js
+++ search/all_8.js
@@ -5,5 +5,6 @@ var searchData=
   ['interval_2ehpp_2',['interval.hpp',['../interval_8hpp.html',1,'']]],
   ['intervalmap_3',['IntervalMap',['../classhsc__snippets_1_1IntervalMap.html',1,'hsc_snippets::IntervalMap&lt; T, V, update_policy_t &gt;'],['../classhsc__snippets_1_1IntervalMap.html#a80c39eaa568b9edbd3724db50b9beecd',1,'hsc_snippets::IntervalMap::IntervalMap()']]],
   ['intervalset_4',['IntervalSet',['../classhsc__snippets_1_1IntervalSet.html',1,'hsc_snippets::IntervalSet&lt; T &gt;'],['../classhsc__snippets_1_1IntervalSet.html#aefb2a5c7f1f4f28cb315c0619d23e2bd',1,'hsc_snippets::IntervalSet::IntervalSet()']]],
-  ['ispalindrome_5',['isPalindrome',['../namespacehsc__snippets.html#a53b3bf79c6d5c2413f3bd31b909b21ba',1,'hsc_snippets']]]
+  ['ispalindrome_5',['isPalindrome',['../namespacehsc__snippets.html#a53b3bf79c6d5c2413f3bd31b909b21ba',1,'hsc_snippets']]],
+  ['isperfectsquare_6',['isPerfectSquare',['../namespacehsc__snippets.html#a7ca516ac018a2770d79d6fe162f497d1',1,'hsc_snippets']]]
 ];
