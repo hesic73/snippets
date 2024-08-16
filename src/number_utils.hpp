402756b80d5906f4ad5d02fa3a4403ed9f58b038
Sicheng He <hesicheng20@gmail.com>
2024-08-16 14:54:09 -0700

    isPerfectSquare

--- src/number_utils.hpp
+++ src/number_utils.hpp
@@ -4,9 +4,10 @@
 #include <concepts>
 #include <cmath>
 #include <vector>
+#include <array>
+#include <cstdint>
 
-namespace hsc_snippets
-{
+namespace hsc_snippets {
 	/**
 	 * Calculates the number of digits in the decimal representation of an integer.
 	 *
@@ -14,12 +15,10 @@ namespace hsc_snippets
 	 * @param num The input number whose number of decimal digits is to be calculated.
 	 * @return The number of digits in the decimal representation of num.
 	 */
-	template <std::integral T>
-	int numDigits(T num)
-	{
+	template<std::integral T>
+	int numDigits(T num) {
 		int cnt = 0;
-		while (num != 0)
-		{
+		while (num != 0) {
 			cnt += 1;
 			num /= 10;
 		}
@@ -35,16 +34,13 @@ namespace hsc_snippets
 	 * @param num The input number whose binary bit count is to be calculated.
 	 * @return The number of bits required to represent num in binary, or the bit size of T for negative numbers.
 	 */
-	template <std::integral T>
-	int numBits(T num)
-	{
-		if (num < 0)
-		{
+	template<std::integral T>
+	int numBits(T num) {
+		if (num < 0) {
 			return sizeof(T) << 3;
 		}
 		int cnt = 0;
-		while (num != 0)
-		{
+		while (num != 0) {
 			cnt += 1;
 			num = num >> 1;
 		}
@@ -59,11 +55,9 @@ namespace hsc_snippets
 	 * @param b The second number.
 	 * @return The greatest common divisor of a and b.
 	 */
-	template <std::integral T>
-	T gcd(T a, T b)
-	{
-		while (b != 0)
-		{
+	template<std::integral T>
+	T gcd(T a, T b) {
+		while (b != 0) {
 			T temp = b;
 			b = a % b;
 			a = temp;
@@ -80,10 +74,15 @@ namespace hsc_snippets
 	 * @param b The second number.
 	 * @return The least common multiple of a and b.
 	 */
-	template <std::integral T>
-	T lcm(T a, T b)
-	{
-		return (a * b) / gcd(a, b);
+	template<std::integral T>
+	T lcm(T a, T b) {
+		T gcd_val = gcd(a, b);
+		T tmp = b / gcd_val;
+		// To avoid overflow: check if multiplication a * (b / gcd_val) would overflow
+		if (a > std::numeric_limits<T>::max() / tmp) {
+			throw std::overflow_error("LCM calculation overflowed");
+		}
+		return a * tmp;
 	}
 
 	/**
@@ -92,8 +91,7 @@ namespace hsc_snippets
 	 * @param n The upper limit (inclusive) up to which prime numbers are to be found.
 	 * @return A std::vector<int> containing all the prime numbers less than or equal to n.
 	 */
-	static std::vector<int> SieveOfEratosthenes(int n)
-	{
+	static std::vector<int> SieveOfEratosthenes(int n) {
 		// Initialize a boolean vector "prime" with entries up to n. All entries are initially set to true.
 		// A value in prime[i] will be false if i is not a prime number, and true if it is a prime number.
 		std::vector<bool> prime(n + 1, true);
@@ -105,15 +103,12 @@ namespace hsc_snippets
 		// Start iterating from the first prime number, 2, up to the square root of n.
 		// We only need to go up to sqrt(n) because if n has a divisor greater than sqrt(n),
 		// it must also have a smaller one, so all composites will have been marked by this point.
-		for (int p = 2; p <= std::sqrt(n); ++p)
-		{
+		for (int p = 2; p <= std::sqrt(n); ++p) {
 			// If prime[p] is true, then it is a prime number.
-			if (prime[p])
-			{
+			if (prime[p]) {
 				// Mark all multiples of p starting from p^2 as not prime.
 				// Starting from p^2 because all smaller multiples of p would have already been marked by smaller primes.
-				for (int i = p * p; i <= n; i += p)
-				{
+				for (int i = p * p; i <= n; i += p) {
 					prime[i] = false;
 				}
 			}
@@ -121,16 +116,35 @@ namespace hsc_snippets
 
 		// Collect all prime numbers: iterate over the range up to n and
 		// add the number to the primes list if its corresponding value in the prime vector is true.
-		for (int p = 2; p <= n; ++p)
-		{
-			if (prime[p])
-			{
+		for (int p = 2; p <= n; ++p) {
+			if (prime[p]) {
 				primes.push_back(p);
 			}
 		}
 
 		// Return the vector containing all the primes found.
 		return primes;
+	}
+
+	/**
+	 * Checks if a given number is a perfect square using a precomputed array of squares.
+	 *
+	 * @param num The number to check if it is a perfect square.
+	 * @return True if num is a perfect square, otherwise false.
+	 */
+	static bool isPerfectSquare(std::int32_t num) {
+		if (num < 0) {
+			return false;
+		}
+		constexpr std::array<std::int32_t, 46341> arr = []()constexpr {
+			std::array<std::int32_t, 46341> _arr = {};
+			for (int i = 0; i < 46341; ++i) {
+				_arr[i] = i * i;
+			}
+			return _arr;
+		}();
+
+		return std::binary_search(arr.begin(), arr.end(), num);
 	}
 }
 
--- test/test_number_utils.cpp
+++ test/test_number_utils.cpp
@@ -3,43 +3,49 @@
 #include <cstdint>
 using namespace hsc_snippets;
 
-TEST_CASE("number_utils.hpp", )
-{
-    SECTION("numDigits")
-    {
+TEST_CASE("number_utils.hpp",) {
+    SECTION("numDigits") {
         REQUIRE(numDigits(0) == 0);
         REQUIRE(numDigits(10) == 2);
         REQUIRE(numDigits(99) == 2);
         REQUIRE(numDigits(12345) == 5);
         REQUIRE(numDigits(-412345) == 6);
     }
-    SECTION("numBits")
-    {
+    SECTION("numBits") {
         REQUIRE(numBits(0) == 0);
         REQUIRE(numBits(1 << 8) == 9);
         REQUIRE(numBits((1 << 8) - 1) == 8);
         REQUIRE(numBits(static_cast<std::int32_t>(-12345)) == 32);
     }
-    SECTION("gcd")
-    {
+    SECTION("gcd") {
         REQUIRE(gcd(6, 4) == 2);
         REQUIRE(gcd(4, 6) == 2);
         REQUIRE(gcd(8, 9) == 1);
         REQUIRE(gcd(24, 36) == 12);
     }
-    SECTION("lcm")
-    {
+    SECTION("lcm") {
         REQUIRE(lcm(6, 4) == 12);
         REQUIRE(lcm(4, 6) == 12);
         REQUIRE(lcm(8, 9) == 72);
         REQUIRE(lcm(24, 36) == 72);
     }
 
-    SECTION("SieveOfEratosthenes"){
+    SECTION("SieveOfEratosthenes") {
         auto primes = SieveOfEratosthenes(100);
-        auto expected_primes=std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                                         31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-                                         73, 79, 83, 89, 97};
+        auto expected_primes = std::vector<int>{
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+            73, 79, 83, 89, 97
+        };
         REQUIRE(primes==expected_primes);
+    }
+
+    SECTION("isPerfectSquare") {
+        REQUIRE(!isPerfectSquare(-12345));
+        REQUIRE(isPerfectSquare(0));
+        REQUIRE(!isPerfectSquare(10));
+        REQUIRE(!isPerfectSquare(99));
+        REQUIRE(isPerfectSquare(100));
+        REQUIRE(!isPerfectSquare(101));
     }
 }
